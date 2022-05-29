8b42f9d9c95b53f3cce24ffc0bd293f4b7973010
yusuftokmak <yusuf.tokmak@grubtech.com>
2022-05-29 13:00:03 +0300

    testing puppeteer

--- functions/create-pdf/create-pdf.js
+++ functions/create-pdf/create-pdf.js
@@ -1,11 +1,9 @@
-const chromium = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer");
 
 const createPdf = async (htmlPage) => {
-  const browser = await chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless,
+  const browser = await puppeteer.launch({
+    args: puppeteer.args,
+    headless: true,
   });
 
   const page = await browser.newPage();
